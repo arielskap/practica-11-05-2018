ae326d2b8fdc4cbef0a4f5a3da5d17af525abcde
arielskap <arielskap@gmail.com>
2018-05-11 11:35:30 -0300

    Add files via upload

---
+++ Practica para el parcial/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//int esPar(int);
+
+int main()
+{
+    /*//1)
+    char letraE[4];
+    for(int i=0;i<5;i++)
+    {
+        letraE[i]='e';
+    }*/
+    /*//2)
+    float precio[18];
+    precio[18]=22.3456;
+    printf("precio= %.2f",precio[17], precio[18]);
+    */
+    /*//3)
+    int a=0, b=1;
+    if(a && b)
+    printf("hola");
+    else
+    printf("chau");
+    return 0;
+    */
+    /*//4)
+    int numero;
+    int validar;
+    printf("Ingrese un numero");
+    scanf("%d",&numero);
+    validar=esPar(numero);
+    if(validar==1)
+    {
+        printf("Es par");
+    }*/
+    //5)Contador es un incremento continuo e igual. Puede ir de 1 en 1, dos en dos, etc. Mientras que acumulador no tiene porque
+    // ser de uno en uno, dos en dos, etc. Sino que al acumulador se le puede cargar 2, luego 4, 1 ,3, etc. Sin necesidad de tener
+    //un orden aparente.
+    /*//6)
+    int num;
+    printf("Ingrese un numero entre 200 y 500 (ambos incluidos)");
+    scanf("%d",&num);
+    if(num>=200 && num<=500)
+        printf("El numero ingresado es: %d",num);
+    else
+        printf("El valor ingresado no esta dentro de los valores");
+        */
+}
+
+/*//4)
+int esPar(int entero)
+{
+    int validar;
+    if(entero%2==0)
+    {
+        validar=1;
+    }
+
+    else
+    {
+        validar=0;
+    }
+    return validar;
+}*/
